36e20159d38f0e77bb01011f9f8a66922c882d9e
Hexagonal <hexagonal@example.org>
2020-05-17 23:39:14 +1000

    Zone! (Not info).

--- worker.js
+++ worker.js
@@ -1,36 +1,36 @@
 const mult = (lhs, rhs) => {
-    let [a, b, c] = [lhs[0] * rhs[0], lhs[1] * rhs[1], lhs[2] * rhs[2]]
-    return [b + a, c - a, b + c]
+  let [a, b, c] = [lhs[0] * rhs[0], lhs[1] * rhs[1], lhs[2] * rhs[2]]
+  return [b + a, c - a, b + c]
 }
 
 let cache = [[0n, 1n, 1n]]
 
 function fib(x) {
-    if (x <= 0) return 0n
+  if (x <= 0) return 0n
 
-    let powers = getBinaryPowers(x)
-    let largestPower = powers[0]
+  let powers = getBinaryPowers(x)
+  let largestPower = powers[0]
 
-    while (largestPower > cache.length - 1) {
-        let t = cache[cache.length - 1]
-        cache.push(mult(t, t))
-    }
+  while (largestPower > cache.length - 1) {
+    let t = cache[cache.length - 1]
+    cache.push(mult(t, t))
+  }
 
-    let triples = powers.map((power) => cache[power])
-    let triple = triples.reduce((acc, x) => mult(acc, x))
-    return triple[1]
+  let triples = powers.map((power) => cache[power])
+  let triple = triples.reduce((acc, x) => mult(acc, x))
+  return triple[1]
 }
 
 function getBinaryPowers(x) {
-    let powers = []
+  let powers = []
 
-    while (x > 0) {
-        let i = Math.floor(Math.log2(x))
-        powers.push(i)
-        x -= 2 ** i
-    }
+  while (x > 0) {
+    let i = Math.floor(Math.log2(x))
+    powers.push(i)
+    x -= 2 ** i
+  }
 
-    return powers
+  return powers
 }
 
 onmessage = e => postMessage(fib(e.data).toLocaleString())
